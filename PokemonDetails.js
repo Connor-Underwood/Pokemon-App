8bc352aa15cdfca481b28e6c227e90b66c389874
Connor Underwood <ctu266667@gmail.com>
2023-04-08 19:16:33 -0500

    Update PokemonDetails.js

--- PokemonDetails.js
+++ PokemonDetails.js
@@ -1,12 +1,85 @@
 import React from 'react';
 import './PokemonDetails.css';
+import { database } from '../firebase';
+import { ref, push, get, remove } from 'firebase/database';
+import { useState } from 'react';
 
 const PokemonDetails = ({ pokemon }) => {
+  const [lengthError, setLengthError] = useState(false);
+  const [teamError, setTeamError] = useState(false);
+  const [removeTeamError, setRemoveTeamError] = useState(false);
+  const [teamCleared, setTeamCleared] = useState(false);
+  const [pokemonAdded, setPokemonAdded] = useState(false);
+
   if (!pokemon) {
     return <div className="no-results">No Pokemon found</div>;
   }
 
   const { name, sprites, types, stats, abilities } = pokemon;
+
+  const checkIfPokemonInTeam = async (pokemonName) => {
+    const pokemonTeamRef = ref(database, 'pokemonTeam');
+    const snapshot = await get(pokemonTeamRef);
+    const pokemonTeam = snapshot.val();
+    if (pokemonTeam) {
+      const pokemonNames = Object.values(pokemonTeam).map((pokemon) => pokemon.name);
+      return pokemonNames.includes(pokemonName);
+    } else {
+      return false;
+    }
+  };
+
+  const ableToAdd = async () => {
+    const pokemonTeamRef = ref(database, 'pokemonTeam');
+    const snapshot = await get(pokemonTeamRef);
+    const pokemonTeam = snapshot.val();
+    if (pokemonTeam) {
+      const pokemonNames = Object.values(pokemonTeam).map((pokemon) => pokemon.name);
+      for (pokemon in pokemonNames) {
+        console.log(pokemon);
+      }
+      if (pokemonNames.length >= 6) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return true;
+    }
+  };
+  
+  
+  
+  const handleAddToTeamClick = async () => {
+    const isPokemonInTeam = await checkIfPokemonInTeam(name);
+    const checkLength = await ableToAdd(name);
+    if (checkLength) {
+      if (!isPokemonInTeam) {
+        push(ref(database, 'pokemonTeam'), {
+          name,
+          image: sprites.front_default,
+          types: types.map(typeObj => typeObj.type.name),
+        });
+        setPokemonAdded(true);
+      } else {
+        setTeamError(true);
+      }
+    } else {
+      setLengthError(true);
+    }
+  };
+
+  const handleClearTeamClick = async () => {
+    const pokemonTeamRef = ref(database, 'pokemonTeam');
+    remove(pokemonTeamRef).then(() => {
+      console.log("remove successful");
+      setTeamCleared(true);
+    }).catch((error) => {
+      console.log("remove failed");
+      console.log(error);
+      setRemoveTeamError(true);
+    })
+  }
 
   return (
     <div className="pokemon-details">
@@ -35,6 +108,32 @@ const PokemonDetails = ({ pokemon }) => {
           </div>
         ))}
       </div>
+      <button className='add-team-button' onClick={handleAddToTeamClick}>Add to Team</button>
+      { pokemonAdded && (
+        <div>Pokemon added!</div>
+      )}
+      {teamError && (
+        <div>
+          <h3>This Pokemon is already on your team!</h3>
+        </div>
+      )}
+      {lengthError && (
+        <div>
+          <h3>Your team already has 6 pokemon!</h3>
+        </div>
+      )}
+
+    <div>
+      <button className='remove-pokemon-button' onClick={handleClearTeamClick}>Clear Team</button>
+      { removeTeamError && (
+        <div>Could not remove team!</div>
+      )}
+      {
+        teamCleared && (
+          <div>Team cleared!</div>
+        )
+      }
+    </div>
     </div>
   );
 };
